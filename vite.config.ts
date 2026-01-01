7a1a61599d7254b9776c29b0dedf79a884865e20
Dragan Okanovic <dragan.okan@gmail.com>
2026-01-01 20:05:12 +0100

    shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // eslint-disable-next-line no-restricted-syntax
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     rollupOptions: {
       output: {
